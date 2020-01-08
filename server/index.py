79a4796823256f4fa1016a2237b46667928cdea6
joelcarlss <jc222mw@student.lnu.se>
2020-01-08 10:48:55 +0100

    removing unnececary code

--- server/index.py
+++ server/index.py
@@ -1,5 +1,3 @@
-from manage_files import *
-from utils import *
 from cross_validation import *
 from algorithm import *
 import numpy as np
@@ -17,48 +15,9 @@ def run(path, n):
 	result = np.argmax(norm_array, axis=1)
 	print(accuracy_score(result, label_list))
 
-	# confusion_matrix(norm_array, label_list, unique_types, accuracy_score(result, label_list), False)
+	confusion_matrix(norm_array, label_list, unique_types, accuracy_score(result, label_list), False)
 
 
-run('Iris/iris.csv', 3)
 run('Iris/iris.csv', 5)
-run('Iris/iris.csv', 10)
-# run('banknote_authentication.csv', 5)
 
-
-def iris():
-	title, data, types = get_data('Iris/iris.csv')
-	label_list, unique_types = get_label(types)
-
-	# Separates data in two categories, test and train data. Test data will be as large as the input percentage of data
-	test_data, test_labels, train_data, train_labels = separate_training_data(data, label_list, 10)
-
-	fit(train_data, train_labels)
-	norm_array = predict(test_data)
-	result = np.argmax(norm_array, axis=1)
-	print(norm_array)
-	print(accuracy_score(result, test_labels))
-
-	confusion_matrix(norm_array, test_labels, unique_types, accuracy_score(result, test_labels), True)
-
-
-# iris()
-
-
-def banknote():
-	title, data, image_class = get_data('banknote_authentication.csv')
-	label_list, unique_types = get_label(image_class)
-
-	# Separates data in two categories, test and train data. Test data will be as large as the input percentage of data
-	test_data, test_labels, train_data, train_labels = separate_training_data(data, label_list, 10)
-
-	fit(train_data, train_labels)
-	norm_array = predict(test_data)
-	result = np.argmax(norm_array, axis=1)
-	print(norm_array)
-	print(accuracy_score(result, test_labels))
-
-	confusion_matrix(norm_array, test_labels, unique_types, accuracy_score(result, test_labels), False)
-
-
-# banknote()
+run('banknote_authentication.csv', 5)
